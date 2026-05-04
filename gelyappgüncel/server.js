d89cc2dc3e7d06d5d1fbe8c9c4c187aad17130c3
Alpyldz <73484916+Alpyldz@users.noreply.github.com>
2026-05-05 00:58:06 +0300

    ilk yükleme

---
+++ gelyappgüncel/server.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const twilio=require("twilio");
+const app=express();
+app.use(bodyParser.json());
+const client=twilio("ACCOUNT_SID","AUTH_TOKEN");
+app.post("/mesaj",async(req,res)=>{
+ const {telefon,mesaj}=req.body;
+ await client.messages.create({
+  from:"whatsapp:+14155238886",
+  to:`whatsapp:+${telefon}`,
+  body:mesaj
+ });
+ res.send("ok");
+});
+app.listen(3000);
